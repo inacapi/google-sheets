b68d40c39c5ba41fd4c5e0c3bdfbee1db2f62cd8
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-01-06 16:54:31 -0300

    Usar @googleapis/sheets para ahorrar espacio

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 import { fetch, CookieJar } from 'node-fetch-cookies'
 import { parse } from 'node-html-parser'
-import { google } from 'googleapis'
+import { sheets as sheets_api, auth as google } from '@googleapis/sheets'
 
 const adfs = 'https://adfs.inacap.cl/adfs/ls/?wtrealm=https://siga.inacap.cl/sts/&wa=wsignin1.0&wreply=https://siga.inacap.cl/sts/&wctx=https%3a%2f%2fadfs.inacap.cl%2fadfs%2fls%2f%3fwreply%3dhttps%3a%2f%2fwww.inacap.cl%2ftportalvp%2fintranet-alumno%26wtrealm%3dhttps%3a%2f%2fwww.inacap.cl%2f'
 const sts = 'https://siga.inacap.cl/sts/'
@@ -74,12 +74,12 @@ export const seccion = async (token, matricula, seccion, periodo) => {
 }
 
 export const subir_datos = async (datos_secciones) => {
-    const auth = new google.auth.GoogleAuth({
+    const auth = new google.GoogleAuth({
         scopes: ['https://www.googleapis.com/auth/spreadsheets'],
         keyFile: 'credentials.json',
     })
     const client = await auth.getClient()
-    const sheets = google.sheets({ version: 'v4', auth: client })
+    const sheets = sheets_api({ version: 'v4', auth: client })
     const rangos = process.env.RANGES.split(',').map(r => r.split('-'))
 
     const datos = []
